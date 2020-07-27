e8f9176bdcc6257c0a6548fa086b9b4bb9786654
Yoni19 <anthony.krief@hotmail.fr>
2020-07-27 10:54:43 +0200

    maj

---
+++ lib/controller.rb
@@ -0,0 +1,13 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  post '/gossips/new/' do
+    Gossip.new(["gossip_author"],["gossip_content"]).save
+  end
+end
