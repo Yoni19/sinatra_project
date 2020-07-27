06b62a23493f368b554fe6d95e9faadef786e7a7
Yoni19 <anthony.krief@hotmail.fr>
2020-07-27 17:28:01 +0200

    maj

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,12 +2,31 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
+  
   get '/gossips/new/' do
     erb :new_gossip
   end
+
   post '/gossips/new/' do
-    Gossip.new(["gossip_author"],["gossip_content"]).save
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
   end
+
+  get '/gossips/:id/' do
+    erb :show, locals: {gossips: Gossip.find(params['id']), id: params['id']}
+  end
+
+
+get '/gossips/:id/edit/' do 
+  id = (params["id"])
+  erb :edit, locals: {gossip: Gossip.find(params['id']), id: params['id']}
+end
+
+post '/gossips/:id/edit/' do
+  id = (params["id"]).to_i
+  Gossip.update(params["gossip_new_author"], params["gossip_new_content"],params['id'].to_i)
+  redirect '/'
+end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,5 +1,6 @@
 require 'pry'
-require 'csv'   
+require 'csv'  
+
 class Gossip
   attr_accessor :author, :content
 
@@ -9,11 +10,37 @@ class Gossip
   end
 
   def save
-    CSV.open("./db/gossip.CSV", "ab") do |csv|
+    CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
   end
 
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+  
+  def self.find(id)
+    Gossip.all[id.to_i]
+  end
+
+  def self.update(new_author,new_content,id)
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << csv_line
+    end
+    all_gossips[id.to_i - 1][0] = new_author
+    all_gossips[id.to_i - 1][1] = new_content
+   CSV.open("./db/gossip.csv", "w") do |gossip_csv|
+    all_gossips.each do |line|
+    gossip_csv << line
+    end 
+  end
+end
 end
 
-binding.pry
+#binding.pry
+
